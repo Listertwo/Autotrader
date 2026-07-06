771557b4eef6d01ac6b449aecfcb4673a88ebee2
Listertwo <98070462+Listertwo@users.noreply.github.com>
2026-07-06 12:38:23 -0400

    Add docstring to GoldenCrossStrategy class
    Added docstring to GoldenCrossStrategy class explaining its purpose and indicators.

--- strategies/trend.py
+++ strategies/trend.py
@@ -2,5 +2,12 @@ from base import Strategy
 from crossover import SmaCrossoverStrategy
 
 class GoldenCrossStrategy(SmaCrossoverStrategy):
+	 """
+    A preset strategy that is based off of the SMA strategy.
+
+	A 1 indicates a Golden Cross -> Bullish Market.
+	A -1 indicates a Death Cross -> Bearish Market.
+    """
+	
 	def __init__(self):
 		super().__init__(50, 200)
