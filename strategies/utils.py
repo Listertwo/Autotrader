3224dc4e9aa16da1e9046f3ed9044ef40caa6161
Listertwo <98070462+Listertwo@users.noreply.github.com>
2026-07-06 12:16:53 -0400

    Refactor crossover functions and add level checks
    Refactor crossover and crossunder functions to accept data series instead of moving average values. Add new functions to check crossover and crossunder against a risk index level.

--- strategies/utils.py
+++ strategies/utils.py
@@ -1,35 +1,73 @@
-def cross_over(fast: int, slow: int) -> bool:
+def cross_over(series1: int, series2: int) -> bool:
      """
      Determines if a crossover has occurred between two moving averages.
 
      Parameters
      ----------
-     fast : int
-         The value of the fast moving average.
-     slow : int
-         The value of the slow moving average.
+     series1 : int
+         The value of the first data series.
+     series2 : int
+         The value of the second data series.
 
      Returns
      -------
      bool
          True if a crossover has occurred, False otherwise.
      """
-     return fast > slow and fast.shift(1) <= slow.shift(1)
+     return series1 > series2 and series1.shift(1) <= series2.shift(1)
 
-def cross_under(fast: int, slow: int) -> bool:
+def cross_under(series1: int, series2: int) -> bool:
     """
     Determines if a crossunder has occurred between two moving averages.
 
     Parameters
     ----------
-    fast : int
-        The value of the fast moving average.
-    slow : int
-        The value of the slow moving average.
+    series1 : int
+        The value of the first data series.
+    series2 : int
+        The value of the second data series.
 
     Returns
     -------
     bool
         True if a crossunder has occurred, False otherwise.
     """
-    return fast < slow and fast.shift(1) >= slow.shift(1)
+    return series1 < series2 and series1.shift(1) >= series2.shift(1)
+
+def cross_over_level(series: int, level: int):
+    """
+    Determines if a crossover has occurred between two moving averages.
+
+    Parameters
+    ----------
+    series : int
+        The value of the data series.
+    series2 : int
+        The value of the risk index level.
+
+    Returns
+    -------
+    bool
+        True if a crossover has occurred, False otherwise.
+    """
+
+	return series > level and series.shift(1) <= level.shift(1)
+
+def cross_under_level(series: int, level: int):
+    """
+    Determines if a crossunder has occurred between two moving averages.
+
+    Parameters
+    ----------
+    series : int
+        The value of the data series.
+    series2 : int
+        The value of the risk index level.
+
+    Returns
+    -------
+    bool
+        True if a crossunder has occurred, False otherwise.
+    """
+
+	return series < level and series.shift(1) >= level.shift(1)
