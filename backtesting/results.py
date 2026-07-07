03a8d658d1eb21e7d72ba4631739ad0f8049aa18
Listertwo <98070462+Listertwo@users.noreply.github.com>
2026-07-07 15:46:47 -0400

    Add metrics for average and largest trades

--- backtesting/results.py
+++ backtesting/results.py
@@ -6,10 +6,18 @@ class BacktestResults:
 	final_cash: float
 
 	total_return: float
+	average_return: float
 
 	trades: int
 
 	wins: int
+	largest_win: float
+	average_win: float
+	winning_trades: list
+	
 	losses: int
+	largest_loss: float
+	average_loss: float
+	losing_trades: list
 
 	win_rate: float
