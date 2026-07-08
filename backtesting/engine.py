5d4471a82207ef720688f0591f4a28bc9735b37a
listertwo <listertwo@yahoo.com>
2026-07-08 06:32:16 -0400

    Added a new feature to the backtesting engine that allows for more accurate simulation of trading strategies. The portfolio management system has been improved to handle multiple assets and dynamic allocation. Additionally, the results and trade models have been reorganized into a separate 'models' directory for better code structure and maintainability.

--- backtesting/engine.py
+++ backtesting/engine.py
@@ -1,143 +1,49 @@
 import pandas as pd
 
 from typing import tuple
+from portfolio import Portfolio
 from strategies.base import Strategy
-from trade import Trade
-from results import BacktestResults
+from models.trade import Trade
+from models.results import BacktestResults
 
 class BacktestEngine:
 	def __init__(self, initial_cash: float = 10000, commission: float = 0.0):
 		self.initial_cash = initial_cash
 		self.commission = commission
 		
-	def _simulate(self, signals: pd.DataFrame) -> tuple[list[Trade], float]:
-		trades = []
-		cash = self.initial_cash
-		shares = 0
-		
-		for index, row in signals.iterrows():
-			row_signal = row["Signal"]
-			row_price = row["Close"]
-
-			if row_signal == 1 and shares == 0:
-				entry_date = index
-				entry_price = row_price
-				cash_after_fee = cash - self.commission
-				
-				shares = cash_after_fee / row_price
-				cash = 0
-				entry_shares = shares
-
-			if row_signal == -1 and shares > 0:
-				exit_date = index
-				exit_price = row_price
-				
-				cash = (shares * row_price) - self.commission
-				shares = 0
-
-				cash_before = entry_shares * entry_price
-				profit = cash - cash_before
-				return_pct = (profit / cash_before) * 100
-
-				holding_period = (exit_date - entry_date).days
-				
-				trades.append(Trade(
-					entry_date = entry_date,
-					exit_date = exit_date,
-					entry_price = entry_price,
-					exit_price = exit_price,
-					shares = entry_shares,
-					commission = self.commission,
-					profit = profit,
-					return_pct = return_pct,
-					holding_period = holding_period
-				))
-				
-				buy_flag = False
-				sell_flag = False
-
-		if shares > 0:
-			exit_date = signals.iloc[-1]["Index"]
-			exit_price = signals.iloc[-1]["Close"]
-			
-			cash = shares * exit_price - self.commission
-			shares = 0
-
-			cash_before = entry_shares * entry_price
-			profit = cash - cash_before
-			return_pct = (profit / cash_before) * 100
-
-			holding_period = (exit_date - entry_date).days
-			
-			trades.append(Trade(
-					entry_date = entry_date,
-					exit_date = exit_date,
-					entry_price = entry_price,
-					exit_price = exit_price,
-					shares = entry_shares,
-					commission = self.commission,
-					profit = profit,
-					return_pct = return_pct,
-					holding_period = holding_period
-				))
-
-		return trades, cash
-
-	def _calculate_results(self, trades: list, cash: float) -> BacktestResults:
-		trade_amount = len(trades)
-		
-		total_return = sum(trade.profit for trade in trades)
-		average_return = (total_return / trade_amount if trade_amount else 0.0)
-		
-		wins = sum(trade.profit > 0 for trade in trades)
-		losses = sum(trade.profit < 0 for trade in trades)
-
-		largest_win = max(trade.profit for trade in trades, default=0.0)
-		largest_loss = min(trade.profit for trade in trades, default=0.0)
-
-		winning_trades = [
-			trade
-			for trade in trades
-			if trade.profit >= 0
-		]
-		average_win = (sum(winning_trades) / len(winning_trades) if winning_trades else 0.0)
-
-		losing_trades = [
-			trade
-			for trade in trades
-			if trade.profit < 0
-		]
-		average_loss = (sum(losing_trades) / len(losing_trades) if losing_trades else 0.0)
-
-		win_rate = (wins / trade_amount if trade_amount else 0.0)
-
-		longest_holding = max(trade.holding_period for trade in trades, default=0.0)
-		shortest_holding = min(trade.holding_period for trade in trades, default=0.0)
-		average_holding = (sum(trade.holding_period) / trade_amount if trade_amount else 0.0)
-		
-		results = BacktestResults(
-			initial_cash = self.initial_cash,
-			final_cash = cash,
-			total_return = total_return,
-			average_return = average_return,
-			trades = trade_amount,
-			wins = wins,
-			largest_win = largest_win,
-			average_win = average_win,
-			winning_trades = winning_trades,
-			losses = losses,
-			largest_loss = largest_loss,
-			average_loss = average_loss,
-			losing_trades = losing_trades,
-			win_rate = win_rate,
-			longest_holding = longest_holding,
-			shortest_holding = shortest_holding,
-			average_holding = average_holding
+	def _simulate(self, signals):
+		portfolio = Portfolio(
+			self.initial_cash,
+			self.commission
 		)
-		
-		return results
+
+		for index, row in signals.iterrows():
+
+			signal = row["Signal"]
+			price = row["Close"]
+
+			if signal == 1:
+				portfolio.buy(index, price)
+
+			elif signal == -1:
+				portfolio.sell(index, price)
+
+			portfolio.snapshot(index, price)
+
+		portfolio.close(
+			signals.index[-1],
+			signals.iloc[-1]["Close"]
+		)
+
+		return portfolio
 	
-	def run(self, strategy: Strategy, df: pd.DataFrame) -> BacktestResults:
+	def run(self, strategy, df):
+
 		signals = strategy.generate_signals(df)
-		trades, cash = self._simulate(signals)
-		return self._calculate_results(trades, cash)
+
+		portfolio = self._simulate(signals)
+
+		return BacktestResults.from_portfolio(
+			portfolio,
+			self.initial_cash
+		)
--- backtesting/portfolio.py
+++ backtesting/portfolio.py
@@ -1,27 +1,188 @@
-from dataclass import dataclass
+from dataclasses import dataclass
+
+from models.trade import Trade
+from models.results import BacktestResults
+
 
 @dataclass
-class ProtfolioSnapshot:
-	date: object
+class PortfolioSnapshot:
+    date: object
+    cash: float
+    shares: float
+    market_value: float
+    equity: float
 
-	cash: float
-	
-	shares: float
-	
-	market_value: float
-	
-	equity: float
 
 class Portfolio:
-	def __init__(self):
-		self.history = []
+	def __init__(self, initial_cash: float, commission: float = 0.0):
+		self.initial_cash = initial_cash
+		self.cash = initial_cash
+		self.commission = commission
 
-	def add_snapshot():
+		self.shares = 0.0
 
-	def equity_curve():
+		self.entry_price = None
+		self.entry_date = None
 
-	def returns():
+		self.trades: list[Trade] = []
+		self.history: list[PortfolioSnapshot] = []
 
-	def drawdowns():
+	@property
+	def has_position(self):
+		return self.shares > 0
 
-	def max_drawdown():
+	def buy(self, date, price):
+
+		if self.has_position:
+			return
+
+		cash_after_fee = self.cash - self.commission
+
+		self.shares = cash_after_fee / price
+		self.cash = 0.0
+
+		self.entry_date = date
+		self.entry_price = price
+
+	def sell(self, date, price):
+
+		if not self.has_position:
+			return
+
+		cash = self.shares * price - self.commission
+
+		cost_basis = self.shares * self.entry_price
+
+		profit = cash - cost_basis
+
+		return_pct = (profit / cost_basis) * 100
+
+		holding_period = (date - self.entry_date).days
+
+		self.trades.append(
+			Trade(
+				entry_date=self.entry_date,
+				exit_date=date,
+				entry_price=self.entry_price,
+				exit_price=price,
+				shares=self.shares,
+				commission=self.commission,
+				profit=profit,
+				return_pct=return_pct,
+				holding_period=holding_period,
+			)
+		)
+
+		self.cash = cash
+		self.shares = 0.0
+
+		self.entry_date = None
+		self.entry_price = None
+
+	def close(self, date, price):
+
+		if self.has_position:
+			self.sell(date, price)
+
+	def snapshot(self, date, price):
+
+		market_value = self.shares * price
+
+		self.history.append(
+			PortfolioSnapshot(
+				date=date,
+				cash=self.cash,
+				shares=self.shares,
+				market_value=market_value,
+				equity=self.cash + market_value,
+			)
+		)
+
+	def equity_curve(self):
+		return [s.equity for s in self.history]
+
+	def returns(self):
+		eq = self.equity_curve()
+
+		if len(eq) < 2:
+			return []
+
+		return [
+		(eq[i] / eq[i - 1]) - 1
+		for i in range(1, len(eq))
+		]
+
+	def drawdowns(self):
+
+		peak = float("-inf")
+		drawdowns = []
+
+		for equity in self.equity_curve():
+
+			peak = max(peak, equity)
+
+			drawdowns.append((equity - peak) / peak)
+
+		return drawdowns
+
+	def max_drawdown(self):
+
+		dd = self.drawdowns()
+
+		return min(dd) if dd else 0.0
+	
+	def from_portfolio(self, portfolio: Portfolio, initial_cash: float) -> BacktestResults:
+		trades = portfolio.trades
+		cash = initial_cash
+
+		trade_amount = len(trades)
+		
+		total_return = sum(trade.profit for trade in trades)
+		average_return = (total_return / trade_amount if trade_amount else 0.0)
+		
+		wins = sum(trade.profit > 0 for trade in trades)
+		losses = sum(trade.profit < 0 for trade in trades)
+
+		largest_win = max((trade.profit for trade in trades), default=0.0)
+		largest_loss = min((trade.profit for trade in trades), default=0.0)
+
+		winning_trades = [
+			trade
+			for trade in trades
+			if trade.profit >= 0
+		]
+		average_win = (sum(winning_trades) / len(winning_trades) if winning_trades else 0.0)
+
+		losing_trades = [
+			trade
+			for trade in trades
+			if trade.profit < 0
+		]
+		average_loss = (sum(losing_trades) / len(losing_trades) if losing_trades else 0.0)
+
+		win_rate = (wins / trade_amount if trade_amount else 0.0)
+
+		longest_holding = max((trade.holding_period for trade in trades), default=0.0)
+		shortest_holding = min((trade.holding_period for trade in trades), default=0.0)
+		average_holding = (sum((trade.holding_period for trade in trades), default=0.0) / trade_amount if trade_amount else 0.0)
+		
+		return BacktestResults.from_portfolio(
+			initial_cash = self.initial_cash,
+			final_cash = cash,
+			total_return = total_return,
+			average_return = average_return,
+			trades = trade_amount,
+			wins = wins,
+			largest_win = largest_win,
+			average_win = average_win,
+			winning_trades = winning_trades,
+			losses = losses,
+			largest_loss = largest_loss,
+			average_loss = average_loss,
+			losing_trades = losing_trades,
+			win_rate = win_rate,
+			longest_holding = longest_holding,
+			shortest_holding = shortest_holding,
+			average_holding = average_holding
+		)
+    
