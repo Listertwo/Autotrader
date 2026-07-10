cdccdfece5fbff9eb63ede7fe3aba8e861e44792
Listertwo <98070462+Listertwo@users.noreply.github.com>
2026-07-10 09:40:10 -0400

    Implement __str__ method in Trade class
    Add __str__ method to Trade class for better string representation.

--- models/trade.py
+++ models/trade.py
@@ -3,6 +3,13 @@ from pandas import Timestamp
 
 @dataclass
 class Trade:
+	def __str__(self) -> str:
+		return(
+			f"{self.entry_date.date()} -> {self.exit_date.date()} | "
+        	f"${self.entry_price:.2f} -> ${self.exit_price:.2f} | "
+        	f"Profit ${self.profit:.2f} ({self.return_pct:.2f}%)"
+		)
+	
 	entry_date: Timestamp
 	exit_date: Timestamp
 
