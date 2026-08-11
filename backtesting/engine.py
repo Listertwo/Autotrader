b34045371dc1dbbacfea42f47f9bd28290dc0192
Listertwo <98070462+Listertwo@users.noreply.github.com>
2026-08-11 16:03:51 -0400

    Refactor backtest engine to use MarketEvents
    Refactored simulation method to use events instead of signals and added event building functionality.

--- backtesting/engine.py
+++ backtesting/engine.py
@@ -5,44 +5,61 @@ from portfolio import Portfolio
 from strategies.base import Strategy
 from models.trade import Trade
 from models.results import BacktestResults
+from models.event import MarketEvent
 
 class BacktestEngine:
 	def __init__(self, initial_cash: float = 10000, commission: float = 0.0):
 		self.initial_cash = initial_cash
 		self.commission = commission
 		
-	def _simulate(self, signals):
+	def _simulate(self, events):
 		portfolio = Portfolio(
 			self.initial_cash,
 			self.commission
 		)
 
-		for index, row in signals.iterrows():
+		for event in events: 
+			if event.signal == 1:
+				portfolio.buy(event.date, event.price)
 
-			signal = row["Signal"]
-			price = row["Close"]
+			elif event.signal == -1:
+				portfolio.sell(event.date, event.price)
 
-			if signal == 1:
-				portfolio.buy(index, price)
-
-			elif signal == -1:
-				portfolio.sell(index, price)
-
-			portfolio.snapshot(index, price)
+			portfolio.snapshot(event.date, event.price)
 
 		portfolio.close(
-			signals.index[-1],
-			signals.iloc[-1]["Close"]
+			events[-1].date,
+			events[-1].price
 		)
 
 		return portfolio
-	
+
+	def _build_events(self, signals):
+		events = []
+
+		for symbol, df in signals.items():
+			for index, row in df.iterrows():
+				events.append(
+					MarketEvent(
+						date = index,
+						symbol = symbol,
+						price = row["Close"],
+						signal = row["Signal"]
+					)
+				)
+
+		events.sort(key=lambda event: event.date)
+
+		return events
+
 	def run(self, strategy, data: dict[str, pd.DataFrame]):
 		
 		signals = {}
 		for symbol, df in data.items():
 			signals[symbol] = strategy.generate_signals(df)
 
-		portfolio = self._simulate(signals)
+		events = self._build_events(signals)
+		
+		portfolio = self._simulate(events)
 
 		return BacktestResults.from_portfolio(portfolio)
